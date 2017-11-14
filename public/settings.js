7667e9a5aefc557bf0d98a5d53400dd3263e98b6
Unknown <chaos67731@gmail.com>
2017-11-13 22:12:26 -0800

    Again

--- public/settings.js
+++
@@ -1,9 +0,0 @@
-// Weather Settings 
-var WeatherLocation = '12795450';
-
-// Calendar Settings ----
-var CalndUse    = 'blank'; // blank // fullCalendar
-// Before Using fullCalendar you will need to set up and get th API key
-// https://fullcalendar.io/docs/google_calendar/
-var CalndAPI    = ' ';
-var CalndID     = ' ';
