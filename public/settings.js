45d7127cd1ed15857c9fd8e0f01d9267559aaeb3
Unknown <chaos67731@gmail.com>
2017-11-22 21:33:50 -0800

    Update Setting

--- public/settings.js
+++ public/settings.js
@@ -1,6 +1,3 @@
 // Calendar Settings ----
-var CalndUse    = 'fullCalendar'; // blank // fullCalendar
-// Before Using fullCalendar you will need to set up and get th API key
-// https://fullcalendar.io/docs/google_calendar/
-var CalndAPI    = 'AIzaSyDXwY5XKNCh5Vxk3hPZ9cczEPX1sWmal94';
-var CalndID     = '7qu74p16lthsf6n8n9mcau5a90@group.calendar.google.com';
+var CalndAPI    = 'AIzaSyCR3-ptjHE-_douJsn8o20oRwkxt-zHStY';
+var CalndID     = 'duv2qiavgjlin85csle7s7megk@group.calendar.google.com';
