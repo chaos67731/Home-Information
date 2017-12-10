aa47946c45e411020fe2f1dcabfd077a69b7160b
Unknown <chaos67731@gmail.com>
2017-12-09 19:50:13 -0800

    Page set up

---
+++ src/pages/CurrentWeatherPage.jsx
@@ -0,0 +1,14 @@
+ import React, { Component } from 'react';
+
+class CurrentWeatherPage extends Component{
+  render(){
+    return(
+      <div className="CurrentWeatherPage"> 
+        My Current Weather Page
+      </div>
+
+    );
+  }
+}
+
+export default CurrentWeatherPage;
