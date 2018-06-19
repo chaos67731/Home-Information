85e4bc4633c6f8aba01529e17b4aafa2d142d94f
James R <james@scalablegroup.com>
2018-06-18 22:40:31 -0700

    Update Bottom Image

--- src/components/BottomImage.jsx
+++ src/components/BottomImage.jsx
@@ -1,18 +1,18 @@
-import React, { Component } from 'react';
-import Clock from './parts/Clock';
+import React, { Component } from "react";
+import Clock from "./parts/Clock";
 
-class BottomImage extends Component{
-	constructor(props) {
-		super(props);
-		this.state = { 
-			deadline: 'April 27, 2018',
-		};
-	}
-	render(){
-    return(
-		<div className="AvengersInfinityWar BottomCountDown" id="BottomImage">
-			 <Clock deadline={ this.state.deadline }/>
-		</div>
+class BottomImage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      deadline: "July 04, 2018"
+    };
+  }
+  render() {
+    return (
+      <div className="FourthOfJuly BottomCountDown" id="BottomImage">
+        <Clock deadline={this.state.deadline} />
+      </div>
     );
   }
 }
