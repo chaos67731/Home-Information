b7bee32a522d3ec83c124d60c403dc37c3c0480f
Unknown <chaos67731@gmail.com>
2018-06-23 15:32:51 -0700

    Auto stash before merge of "master" and "origin/master"

--- public/settings.js
+++ public/settings.js
@@ -1,3 +1,3 @@
 // Calendar Settings ----
-var CalndAPI    = 'AIzaSyCR3-ptjHE-_douJsn8o20oRwkxt-zHStY';
-var CalndID     = 'duv2qiavgjlin85csle7s7megk@group.calendar.google.com';
+var CalndAPI    = 'AIzaSyDXwY5XKNCh5Vxk3hPZ9cczEPX1sWmal94';
+var CalndID     = '7qu74p16lthsf6n8n9mcau5a90@group.calendar.google.com';
